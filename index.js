8a35188a3de76f476a2703ccebe438be5fa052dc
João Pedro <estudante2025@gmail.com>
2022-03-14 19:36:45 -0300

    Bootstrap instalado

--- index.js
+++ index.js
@@ -3,16 +3,10 @@ const app = express()
 
 // Estou dizendo para o express usar o EJS como view engine.
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
-app.get("/:nome/:lang", (req, res) => {
-    let nome = req.params.nome
-    let lang = req.params.lang
-    res.render("index", {
-        nome: nome,
-        lang: lang,
-        empresa: "Pedro LTDA",
-        inscritos: 8000
-    })
+app.get("/", (req, res) => {
+    res.render("index")
 })
 
 app.listen(8080, ()=> {
