ab01cb57b20cd472eff82dbc510ccc0824e0fe81
João Pedro <estudante2025@gmail.com>
2022-03-18 21:31:31 -0300

    Envio de dados iniciado

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const express = require("express")
 const app = express()
+const bodyParser = require("body-parser")
 
 // Estou dizendo para o express usar o EJS como view engine.
 app.set('view engine', 'ejs')
+// setando as sources
 app.use(express.static('public'))
+// Traduzindo para uma estrutura Javascript
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
+
+// Rotas 
 app.get("/", (req, res) => {
     res.render("index")
 })
 
 app.get("/perguntar", (req, res) => {
     res.render("perguntar")
+})
+
+app.post("/salvarpergunta", (req, res) => {
+    var titulo = req.body.titulo
+    var descricao = req.body.descricao
+    res.send(`Formulário recebido! <br>Título: ${titulo} <br>Descrição: ${descricao}`)
 })
 
 app.listen(8080, ()=> {
