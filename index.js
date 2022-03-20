b1e00a7450ca33751beddeaa08f6d0905f14d905
João Pedro <estudante2025@gmail.com>
2022-03-20 01:37:29 -0300

    Tabela perguntas criada e conectada com o formulário.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
 const connection = require("./database/database")
+const Pergunta = require("./database/Pergunta")
 // Database
 
 connection
@@ -34,7 +35,13 @@ app.get("/perguntar", (req, res) => {
 app.post("/salvarpergunta", (req, res) => {
     var titulo = req.body.titulo
     var descricao = req.body.descricao
-    res.send(`Formulário recebido! <br>Título: ${titulo} <br>Descrição: ${descricao}`)
+    
+    Pergunta.create({
+        titulo: titulo,
+        descricao: descricao
+    }).then(() => {
+        res.redirect("/")
+    })
 })
 
 app.listen(8080, ()=> {
